fcb336d357f57855a3a6976470ccf818132099fe
Johan <contatojohan@gmail.com>
2018-08-22 12:42:57 -0300

    teste

--- server.js
+++ server.js
@@ -1,46 +1,24 @@
-﻿'use strict';
+﻿var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var x = 'inicial';
 
-const express = require('express')
-const app = express()
-const server = require('http').createServer(app).listen(5000)
-const io = require('socket.io').listen(server)
-const bodyParser = require('body-parser');
-  
-	app.use(bodyParser.urlencoded({ extended: true }));
-	app.use(bodyParser.json());
+app.set('port',(process.env.PORT || 5000));
 
-	const port = process.env.PORT || 3000;
+app.get('/', function(req, res) {
+res.sendFile(__dirname + "/index.html");
+});
 
-	const router = express.Router();
+io.on('connection', function (socket) {
+x = socket;
+});
 
-/* Vamos colocar nosso Middleware aqui, depois */
-	var noMeio = function(req, res, next){
-	var token = req.query.token || ''; 
-	var loja = req.query.loja || ''; 
-	var number = req.query.number || ''; 
-	var msg = req.query.msg|| ''; 
-	if(token == 'tokencode'){	
-		io.emit('notificacao', {loja: ''+loja+'', number: ''+number+'', msg: ''+msg+''});
-	console.log(msg);
-		next();
-	} else {
-		res.status(401).json({
-			aviso: 'Erro!',
-			message: 'Você não possui acesso.'
-		})
-		}
-};
+app.get('/sendevent', function(request, response) {
+console.log('XXXXX');
+x.emit('news', {hello: 'cachvache'});
+response.send('FINITO');
+});
 
-	app.use('/', router);
-
-	router.route('/api') //inserimos noMeio como primeiro parâmetro
-		.get(noMeio, function(req, res){
-			if(!res.data){ res.data = {}; }
-			res.data.message = "success"
-			res.json(res.data);
-			
-	});
-
-
-	app.listen(port);
-	console.log('conectado a porta ' + port);
+http.listen(app.get('port'), function(){
+console.log('listening on:'+app.get('port'));
+});
