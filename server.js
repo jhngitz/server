9e0aa2994e5ba23845597b8830cc533123570ed6
Johan <contatojohan@gmail.com>
2018-08-22 11:25:42 -0300

    att port

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
-﻿var express = require('express')
-  , app = express()
-  , server = require('http').createServer(app).listen(4555)
-  , io = require('socket.io').listen(server)
-  , bodyParser = require('body-parser');
+﻿'use strict';
+
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app).listen(5000)
+const io = require('socket.io').listen(server)
+const bodyParser = require('body-parser');
   
 	app.use(bodyParser.urlencoded({ extended: true }));
 	app.use(bodyParser.json());
 
-	var port = process.env.PORT || 5000;
+	const port = process.env.PORT || 3000;
 
-	var router = express.Router();
+	const router = express.Router();
 
 /* Vamos colocar nosso Middleware aqui, depois */
-var noMeio = function(req, res, next){
+	var noMeio = function(req, res, next){
 	var token = req.query.token || ''; 
 	var loja = req.query.loja || ''; 
 	var number = req.query.number || ''; 
